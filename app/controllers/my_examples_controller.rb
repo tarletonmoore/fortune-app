950382ba466608ce8b3665a022621d66ff02e372
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-07-28 12:59:25 -0400

    add fortune and numbers method

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,12 @@
 class MyExamplesController < ApplicationController
+  def fortune
+    fortunes = ["You will find money", "you will perish", "Your dreams will come true"]
+    render json: fortunes.sample
+  end
+
+  def numbers
+    number = (1..60).each { |num| puts num }
+
+    render json: number
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/fortune", controller: "my_examples", action: "fortune"
+  get "/numbers", controller: "my_examples", action: "numbers"
 
   # Defines the root path route ("/")
   # root "articles#index"
